c3eb1d856e10eece65d5d03b841950edd5124633
Ryder Belserion <52214219+RyderBelserion@users.noreply.github.com>
2022-08-02 23:13:11 -0400

    Now lookie look

--- src/main/java/com/badbones69/epicsellchest/commands/WandCommand.java
+++ src/main/java/com/badbones69/epicsellchest/commands/WandCommand.java
@@ -12,24 +12,40 @@ import org.bukkit.entity.Player;
 
 import java.util.HashMap;
 
-@Command(value = "sellchest", alias = {"sc"})
 public class WandCommand extends SellChestCmdNew {
     
-    private CrazyManager crazyManager = CrazyManager.getInstance();
+    private final CrazyManager crazyManager = CrazyManager.getInstance();
     
-    @Default(alias = {"wand", "w"})
+    @Default
     @Permission("epicsellchest.wand")
-    public void wandCommand(CommandSender sender) {
-        wandCommand(sender, 0, null);
+    // Using "Player player" forces it to be a player only command without needing to check if it's a player or not.
+    // All handled for you & it uses the message key you defined in EpicSellChest.
+    public void defaultWandCommand(Player player) {
+        player.getInventory().addItem(crazyManager.getChestSellingItem(1));
+        player.updateInventory();
+
+        //HashMap<String, String> placeholders = new HashMap<>();
+        //placeholders.put("%player%", player.getName());
+        //placeholders.put("%amount%", amount + "");
+        //player.sendMessage(Messages.WAND_GIVE.getMessage(placeholders));
+
+        // TODO() New message for being given a wand.
     }
     
     @SubCommand(value = "wand", alias = {"w"})
     @Permission("epicsellchest.wand")
-    public void wandCommand(CommandSender sender, int amount, @ArgName("player") Player target) {
-        if (target == null && !(sender instanceof Player)) {
-            sender.sendMessage(Messages.PLAYER_ONLY.getMessage());
-            return;
-        }
+    // Using "CommandSender sender" allows it to run in console.
+    // Should already check if Player target is null.
+    // Check above comment, so you don't have to check if sender is instanceof Player.
+    public void wandCommandOthers(CommandSender sender, int amount, @ArgName("player") Player target) {
+        //if (target == null && !(sender instanceof Player)) {
+        //    sender.sendMessage(Messages.PLAYER_ONLY.getMessage());
+        //    return;
+        //}
+        // This commented out code shouldn't be needed because of this in EpicSellChest main class.
+        // commandManager.registerMessage(BukkitMessageKey.PLAYER_ONLY, (sender, context) -> sender.sendMessage(Messages.MUST_BE_A_PLAYER.getMessage()));
+        // commandManager.registerMessage(MessageKey.INVALID_ARGUMENT, (sender, context) -> sender.sendMessage(Messages.NOT_ONLINE.getMessage().replace("%player%", context.getTypedArgument())));
+
         target.getInventory().addItem(crazyManager.getChestSellingItem(amount));
         target.updateInventory();
         HashMap<String, String> placeholders = new HashMap<>();
@@ -37,5 +53,4 @@ public class WandCommand extends SellChestCmdNew {
         placeholders.put("%amount%", amount + "");
         sender.sendMessage(Messages.WAND_GIVE.getMessage(placeholders));
     }
-    
 }
